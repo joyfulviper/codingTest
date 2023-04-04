130e795904479e8332ab9485da23763ba7b04542
doyeon <kimsk3113@naver.com>
2023-04-04 23:55:07 +0900

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Solution s = new Solution();
+        s.solution();
+    }
+}
+
+class Solution {
+
+    public void solution() {
+
+    }
+}
+
