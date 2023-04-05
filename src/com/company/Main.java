1a7d4bd350257c37f3711e7817ba167af1c6c1ee
doyeon <kimsk3113@naver.com>
2023-04-05 15:42:25 +0900

    4/5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,7 +14,25 @@ public class Main {
 class Solution {
 
     public void solution() {
+        int[] sequence = {2, 3, -6, 1, 3, -1, 2, 4};
+        //다이나믹 프로그래밍 점화식: sum[i] = sum[i - 1] + sequence[i], i까지의 합중 가장 큰값 더하기
+        long[] sum = new long[sequence.length + 1];
+        for (int i = 1; i < sum.length; i++) {
+            if (i % 2 == 0) {
+                sum[i] = sum[i - 1] + sequence[i - 1];
+            } else {
+                sum[i] = sum[i - 1] + sequence[i - 1] * -1;
+            }
+        }
+        long max = Long.MIN_VALUE;
+        long min = Long.MAX_VALUE;
 
+        for (int i = 0; i < sum.length; i++) {
+            max = Math.max(max, sum[i]);
+            min = Math.min(min, sum[i]);
+        }
+        long answer= max - min;
+        System.out.println(answer);
     }
 }
 
