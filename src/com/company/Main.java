7fe53fa93f8a99402ef2eb86e65fe1943fb19dea
doyeon <kimsk3113@naver.com>
2023-04-12 22:29:53 +0900

    04/12

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,7 +14,30 @@ public class Main {
 class Solution {
 
     public void solution() {
+        int storey = 1952;
+        int sum = 0;
 
+        while (storey > 0) {
+            int oneDigit = storey % 10;
+            int tenDigit = (storey / 10) % 10;
+
+            if (oneDigit > 5) {
+                sum += 10 - oneDigit;
+                storey += 10;
+            } else if (oneDigit == 5) {
+                sum += 5;
+                if (tenDigit >= 5) {
+                    storey += 10;
+                }
+            } else {
+                sum += oneDigit;
+            }
+            storey /= 10;
+        }
+        System.out.println(sum);
     }
+
+
+
 }
 
