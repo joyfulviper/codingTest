d1bcabf5ec65242335e8a61b6ee4e237852403c1
doyeon <kimsk3113@naver.com>
2023-04-14 15:24:59 +0900

    04/14

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Solution s = new Solution();
         s.solution();
     }
@@ -14,7 +14,25 @@ public class Main {
 class Solution {
 
     public void solution() {
+        int[][] targets = {{4, 5}, {4, 8}, {10, 14}, {11, 13}, {5, 12}, {3, 7}, {1, 4}};
 
+        Arrays.sort(targets, (o1, o2) -> Integer.compare(o2[0], o1[0]));
+
+        int start = targets[0][0];
+
+        int answer = 1;
+        for (int i = 1; i <targets.length; i++) {
+            int[] target = targets[i];
+
+            int nowStart = target[0];
+            int nowEnd = target[1];
+
+            if (nowEnd <= start) {
+                answer++;
+                start = nowStart;
+            }
+        }
+        System.out.println(answer);
     }
 }
 
