5c5c8443027d4f76423c2c997214a7d02625b1e4
doyeon <kimsk3113@naver.com>
2023-04-14 15:13:23 +0900

    04/14

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,6 +14,17 @@ public class Main {
 class Solution {
 
     public void solution() {
+        int r1 = 2;
+        int r2 = 5;
+
+        long answer = 0;
+        for (int i = 1; i <= r2; i++) {
+            long start = (long) Math.ceil(Math.sqrt((long) r1 * r1 - (long) i * i)); // 작은 원 바깥쪽
+            long end = (long) Math.floor(Math.sqrt((long) r2 * r2 - (long) i * i));// 큰 원 안쪽
+            answer += (end - start) + 1;
+        }
+        answer *= 4;
+        System.out.println(answer);
 
     }
 }
