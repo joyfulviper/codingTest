c7d344f09220a88a600259dbbe4a28f9636b4a7b
doyeon <kimsk3113@naver.com>
2023-04-15 22:45:54 +0900

    04/15

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,7 +14,29 @@ public class Main {
 class Solution {
 
     public void solution() {
+        int n = 7;
+        int[] enemy = {4, 2, 4, 5, 3, 3, 1};
+        int k = 3;
+        int sum = 0;
+        //List<Integer> pre = new LinkedList<>();
 
+        PriorityQueue<Integer> pre = new PriorityQueue<>(Collections.reverseOrder());
+        int count = 0;
+        for (int e : enemy) {
+            sum += e;
+            pre.add(e);
+            if (sum > n) {
+                if (k > 0) {
+                    k--;
+                    int subtract = pre.poll();
+                    sum -= subtract;
+                } else {
+                    break;
+                }
+            }
+            count++;
+        }
+        System.out.println(count);
     }
 }
 
