eb9cdc968b75ac882d942b57a2cd9d93193eb936
doyeon <kimsk3113@naver.com>
2023-05-01 17:52:54 +0900

    05/01

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,7 +14,24 @@ public class Main {
 class Solution {
 
     public void solution() {
-
+        int answer = 0;
+        long[] dp = new long[5001];
+        dp[1] = 0;
+        dp[2] = 3;
+        dp[3] = 0;
+        dp[4] = 11;
+        //dp[6] = dp[4] * dp[2] + dp[2] * 2 + 2
+        //dp[8] = dp[6] * dp[2] + dp[4] * 2 + dp[2] * 2 + 2
+        for (int i = 6; i <= 5000; i+=2) {
+            dp[i] = dp[i - 2] * dp[2];
+            for (int j = 4; j < i; j += 2) {
+                dp[i] += dp[i - j] * 2;
+            }
+            dp[i] += 2;
+            dp[i] %= 1000000007;
+        }
+        answer = (int) dp[8];
+        System.out.println(dp[8]);
     }
 }
 
