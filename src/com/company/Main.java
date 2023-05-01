8be5d228f814c46edaa4b2209da92c8cca27266f
doyeon <kimsk3113@naver.com>
2023-05-01 18:04:40 +0900

    05/01

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,7 +14,21 @@ public class Main {
 class Solution {
 
     public void solution() {
-
+        int n = 5;
+        long[] dp = new long[n + 1];
+        int[] money = {1, 2, 5};
+        for (int i = 0; i <= n; i++) {
+            if(i % money[0] == 0) {
+                dp[i] = 1;
+            }
+        }
+        for (int i = 1; i < money.length; i++) {
+            for (int j = money[i]; j <= n; j++) {
+                dp[j] += dp[j - money[i]];
+            }
+            dp[i] %=  1000000007;
+        }
+        System.out.println(dp[n]);
     }
 }
 
